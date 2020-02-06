75b83db3c617af7080bc786d22fe588024a00ac6
stoneff <837815942@qq.com>
2020-02-06 12:34:12 +0000

    Chapter7 before exercise

--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,9 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    Name_Max_Length = 128
+
+    name = models.CharField(max_length=Name_Max_Length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context
@@ -66,3 +69,25 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+def add_category(request):
+    form = CategoryForm()
+
+    # request.post is the name?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # check if we provide a valid form
+        if form.is_valid():
+            # Save the new category to database.
+            form.save(commit=True)
+            # The category is saved, we can confirm this.
+            # For now, just redirect the user back to the index view.
+            return redirect('/rango/')
+        else:
+            # The supplied form contained errors
+            # just print them to the terminal.
+            print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
