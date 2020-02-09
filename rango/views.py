fd7dc9be1f800819c87e4cc016ec2875827f203c
stoneff <837815942@qq.com>
2020-02-09 21:44:18 +0000

    Chapter9 finished with test

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
-from django.cintrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -71,6 +71,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -94,6 +95,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -190,7 +192,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 
 @login_required
